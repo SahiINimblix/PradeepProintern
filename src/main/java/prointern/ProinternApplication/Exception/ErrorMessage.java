a62a7df8385390979abba74a5c62cdf476e94f3d
SonaliSahoo0408 <sonalisahoonpr91@gmail.com>
2025-09-12 15:42:34 +0530

    10th commit fixing errors of exception package

--- src/main/java/prointern/ProinternApplication/Exception/ErrorMessage.java
+++ src/main/java/prointern/ProinternApplication/Exception/ErrorMessage.java
@@ -14,43 +14,4 @@ public class ErrorMessage {
 	private String message;
 	private String description;
 
-	public ErrorMessage(int StatusCode, Date timestamp, String message, String description) {
-		// TODO Auto-generated constructor stub
-		this.StatusCode = StatusCode;
-		this.timestamp = timestamp;
-		this.message = message;
-		this.description = description;
-	}
-
-	public int getStatusCode() {
-		return StatusCode;
-	}
-
-	public void setStatusCode(int statusCode) {
-		StatusCode = statusCode;
-	}
-
-	public Date getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(Date timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
 }
